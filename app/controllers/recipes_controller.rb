ec6a8ad1ae57bae3ec3da3ab5af99565783e79ca
raza-jafri <arjafri@gmail.com>
2014-11-27 14:17:29 -0600

    Committ 2
    inclusion of API

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'json'
+
 class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
@@ -53,4 +56,35 @@ class RecipesController < ApplicationController
 
     redirect_to "/recipes", :notice => "Recipe deleted."
   end
+
+  def search
+    @ingredient_1 = params[:a]
+    @ingredient_2 = params[:b]
+
+    ing_1 = URI.encode(@ingredient_1)
+    ing_2 = URI.encode(@ingredient_2)
+
+    url = "http://api.yummly.com/v1/api/recipes?_app_id=03dad7a0&_app_key=0e386e167b90fc69b5956a4bd1ab0a6d&q=#{ing_1}+#{ing_2}"
+    raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
+    @matches = parsed_data["matches"]
+    @number_of_matches = @matches.length
+
+    @first = @matches[0]
+    @recipe = @first["recipeName"]
+    @time_to_cook = @first["totalTimeInSeconds"]
+    @big_picture = @first["imageUrlsBySize"][0]
+    @small_picture = @first["smallImageUrls"][0]
+    @rating = @first["rating"]
+    @total_ingredients = @first["ingredients"]
+    @attributes = @first["attributes"]
+
+    render 'index'
+
+  end
+
+
+
+
+
 end
